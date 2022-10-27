c42528ed149f7cc83d77e5fe7f35f0827fe9c4fd
NeyrisBH <113575840+NeyrisBH@users.noreply.github.com>
2022-10-27 01:45:12 -0500

    version1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ClientesService } from './clientes.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'innova-registros';
+export class AppComponent implements OnInit {
+  clientes: any[] = [];
+
+  constructor(
+    private clienteService: ClientesService
+  ) {}
+  ngOnInit(): void {
+    this.clienteService.getAll()
+    .subscribe((data: any) => this.clientes = data._embedded.clientes);
+  }
 }
---
+++ src/app/clientes.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+const API_BASE = 'http://localhost:8080';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ClientesService { 
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getAll() {
+    return this.http.get(`${API_BASE}/clientes`);
+  }
+
+  create(data: any) {
+    return this.http.post(`${API_BASE}/clientes`, data);
+  }
+}
